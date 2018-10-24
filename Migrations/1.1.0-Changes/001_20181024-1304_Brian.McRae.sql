71763ed6b610ee0fc3a9af6a69623dc508103db7
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-10-24 13:05:30 -0500

    Person Table and Address Type Data Change

---
+++ Migrations/1.1.0-Changes/001_20181024-1304_Brian.McRae.sql
@@ -0,0 +1,40 @@
+﻿-- <Migration ID="3ae99a11-74e5-4610-a492-30bf04beb302" />
+GO
+
+PRINT N'Altering [Person].[Person]'
+GO
+ALTER TABLE [Person].[Person] ADD
+[twitter] [varchar] (100) NULL
+GO
+
+SET IMPLICIT_TRANSACTIONS, NUMERIC_ROUNDABORT OFF;
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, NOCOUNT, QUOTED_IDENTIFIER ON;
+
+SET DATEFORMAT YMD;
+
+
+GO
+IF (SELECT COUNT(*)
+    FROM   [Person].[AddressType]) = 0
+    BEGIN
+        PRINT (N'Add 7 rows to [Person].[AddressType]');
+        SET IDENTITY_INSERT [Person].[AddressType] ON;
+        INSERT  INTO [Person].[AddressType] ([AddressTypeID], [Name], [Comments])
+        VALUES                             (1, 'Billing', NULL);
+        INSERT  INTO [Person].[AddressType] ([AddressTypeID], [Name], [Comments])
+        VALUES                             (2, 'Home', NULL);
+        INSERT  INTO [Person].[AddressType] ([AddressTypeID], [Name], [Comments])
+        VALUES                             (3, 'Main Office', NULL);
+        INSERT  INTO [Person].[AddressType] ([AddressTypeID], [Name], [Comments])
+        VALUES                             (4, 'Primary', NULL);
+        INSERT  INTO [Person].[AddressType] ([AddressTypeID], [Name], [Comments])
+        VALUES                             (5, 'Shipping', NULL);
+        INSERT  INTO [Person].[AddressType] ([AddressTypeID], [Name], [Comments])
+        VALUES                             (6, 'Archive', NULL);
+        INSERT  INTO [Person].[AddressType] ([AddressTypeID], [Name], [Comments])
+        VALUES                             (11, 'Regional Office', 'SomeText');
+        SET IDENTITY_INSERT [Person].[AddressType] OFF;
+    END
+
+
+GO
---
+++ Programmable Objects/Person/Stored Procedures/GetPersonTwitter.sql
@@ -0,0 +1,14 @@
+﻿IF OBJECT_ID('[Person].[GetPersonTwitter]') IS NOT NULL
+	DROP PROCEDURE [Person].[GetPersonTwitter];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+
+CREATE PROCEDURE [Person].[GetPersonTwitter]
+AS
+SELECT * FROM Person.Person
+GO
