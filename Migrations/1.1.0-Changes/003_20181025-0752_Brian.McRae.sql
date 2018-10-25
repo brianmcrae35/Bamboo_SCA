37a0182030aa83aa4cea888633874c0f145e1bbb
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-10-25 07:52:59 -0500

    Deleted row of AddressType Data

---
+++ Migrations/1.1.0-Changes/003_20181025-0752_Brian.McRae.sql
@@ -0,0 +1,15 @@
+﻿-- <Migration ID="53f6308c-050c-49e4-8024-a5c99d3fba0f" />
+GO
+
+
+SET DATEFORMAT YMD;
+
+
+GO
+PRINT (N'Delete 1 row from [Person].[AddressType]');
+
+DELETE [Person].[AddressType]
+WHERE  [AddressTypeID] = 11;
+
+
+GO
