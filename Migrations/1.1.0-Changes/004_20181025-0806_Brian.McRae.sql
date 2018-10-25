747a6beb6e0a45155b3db69660eeaeb9266a977d
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-10-25 08:06:52 -0500

    Testing Only

---
+++ Migrations/1.1.0-Changes/004_20181025-0806_Brian.McRae.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="58640661-9b7a-4de2-9a40-5dbc0a62688e" />
+GO
+
+PRINT N'Altering [Person].[AddressType]'
+GO
+ALTER TABLE [Person].[AddressType] ADD
+[Comments2] [nvarchar] (255) NULL
+GO
--- Schema-Model/Person/Tables/AddressType.sql
+++ Schema-Model/Person/Tables/AddressType.sql
@@ -2,7 +2,8 @@ CREATE TABLE [Person].[AddressType]
 (
 [AddressTypeID] [int] NOT NULL IDENTITY(1, 1),
 [Name] [varchar] (250) NOT NULL,
-[Comments] [varchar] (100) NULL
+[Comments] [varchar] (100) NULL,
+[Comments2] [nvarchar] (255) NULL
 )
 GO
 ALTER TABLE [Person].[AddressType] ADD CONSTRAINT [PK_AddressType_ID] PRIMARY KEY CLUSTERED  ([AddressTypeID])
