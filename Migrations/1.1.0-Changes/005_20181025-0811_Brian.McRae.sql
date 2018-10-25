5c32d933ac693d3e6645cb3250cc8190d66b479a
RED-GATE\Brian.McRae <brian.mcrae@red-gate.com>
2018-10-25 08:13:12 -0500

    AddressType back to normal state

---
+++ Migrations/1.1.0-Changes/005_20181025-0811_Brian.McRae.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="42aaaaf4-2900-4300-b0c9-f5333ff338e3" />
+GO
+
+PRINT N'Altering [Person].[AddressType]'
+GO
+ALTER TABLE [Person].[AddressType] DROP
+COLUMN [Comments2]
+GO
--- Schema-Model/Person/Tables/AddressType.sql
+++ Schema-Model/Person/Tables/AddressType.sql
@@ -2,8 +2,7 @@ CREATE TABLE [Person].[AddressType]
 (
 [AddressTypeID] [int] NOT NULL IDENTITY(1, 1),
 [Name] [varchar] (250) NOT NULL,
-[Comments] [varchar] (100) NULL,
-[Comments2] [nvarchar] (255) NULL
+[Comments] [varchar] (100) NULL
 )
 GO
 ALTER TABLE [Person].[AddressType] ADD CONSTRAINT [PK_AddressType_ID] PRIMARY KEY CLUSTERED  ([AddressTypeID])
